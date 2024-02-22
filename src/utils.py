818c4c69c4f94c30d13e94963f0fb145d5c12314
Michael Rivera <mriver15@live.com.mx>
2024-02-21 23:01:32 -0600

    About 30% Done, Added most network configuration

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ Holds meta functions, used by predict.py & train.py
 '''
 import argparse
 
+
 def train_args():
     '''
     Retrieves training parameters
@@ -13,8 +14,9 @@ def train_args():
     args.add_argument('--data', help='Training data directory')
     args.add_argument('-a', '--arch', required=True,  help='Architecture: densenet121 or vgg16')
     args.add_argument('-o', '--save_dir', required=True, help='Set directory to save checkpoints')
-    args.add_argument('-l', '--learning_rate', required=False, help='Set hyperparameters: Learn Rate')
+    args.add_argument('-l', '--learning_rate', required=False, help='Set hyperparameters: Learn Rate, defaults to 0.0025')
     args.add_argument('-hu', '--hidden_units', required=False, help='Set hyperparameters: Hidden Units')
-    args.add_argument('-e', '--epochs', required=False, help='Set hyperparameters: Epochs')
+    args.add_argument('-e', '--epochs', required=False, help='Set hyperparameters: Epochs, default to 1')
     args.add_argument('-d', '--gpu', required=False, help='Flag to use GPU for training')
-    return args
+    args.add_argument('-pe', '--print_every', required=False, help='Printout counter, every how many iterations to print out stats')
+    return args.parse_args()
--- train.py
+++ train.py
@@ -1,27 +1,141 @@
-from src import utils
-# from src import dependencies
-
-
-
 # @author mriver15 Michael Rivera
 # Trains a network on a dataset and saves the model as a checkpoint.
 
+# Local Resources & Functions
+from src import utils
+
+# Import nn + optim plus functional from nn
+# makes it easy to access in shorthand, not sure if this will deteriorate performance
+import torch
+from torch import nn, optim
+from torchvision import datasets, transforms, models
+
+# Time & Os
+import time
+from os.path import isdir
+from datetime import datetime
+
+# Data Structures
+from collections import OrderedDict
+import json
+
 
 def main():
+    """
+    Ideally orchestrates training and gathers data as required for training to be done
+    """
+    time_start = time.gmtime()
+    print("---- Training Started ----")
+    # Retrieves arguments from cli
     args = utils.train_args()
-    print(args)
+    
+    # Training Data
+    train_dl, valid_dl = get_dataloaders(args.data)
+    dls = {
+        "train": train_dl, "valid": valid_dl
+    }
 
-def train(model, dataloader, print_frequency, use_gpu):
+    # loads model based on architecture
+    if args.arch == 'densenet121':
+        model = models.densenet121(pretrained=True)
+    elif args.arch == 'vgg16':
+        model = models.vgg16(pretrained=True)
+    else:
+        print("{} Is not a valid Architecture".format(args.arch))
+        exit(0)
+
+    # Classifier definition
+    model.classifier = get_classifier(args.hidden_units)
+    model.criterion = nn.NLLLoss()
+    if args.learning_rate:
+        model.optimizer = optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
+    else:
+        model.optimizer = optim.Adam(model.classifier.parameters(), lr=.0025)
+
+def get_classifier(model, hidden_units):
+    """
+    Retrieves classifier using custom in_units based on model and hidden_units
+    @param model Either vgg16 or densenet121
+    @param hidden_units preferably a number between 1024 & 180 or a number
+        between 25088 & 180
+    """
+    in_units = 1024
+    if model == "vgg16":
+        in_units = 25088
+    classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(in_units, hidden_units)),
+                          ('relu', nn.ReLU()),
+                          ('fc2', nn.Linear(hidden_units, 180)),
+                          ('relu', nn.ReLU()),
+                          ('droupout',nn.Dropout(0.5)),
+                          ('fc3', nn.Linear(180, 102)),
+                          ('relu', nn.ReLU()),
+                          ('fc4', nn.Linear(102, 102)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+    return classifier
+
+
+
+def get_device(device):
+    """
+    Checks for gpu availability if requested
+    @param device cpu or gpu
+    """
+    if not device:
+        return torch.device("cpu")
+    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def get_category_mapping():
+    """
+        Retrieves cat_to_name file
+        contains mapping of category ID to the name of the flower category
+    """
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+    return cat_to_name
+
+
+def get_dataloaders(img_dir):
+    """
+    Retrieves and prepares training & validation data images
+    @param img_dir image directory for training
+    """
+    train_transform = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+    valid_transform = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+    
+    train_dataset = datasets.ImageFolder(img_dir, transform=train_transform) 
+    valid_dataset = datasets.ImageFolder(img_dir, transform=valid_transform)
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64,shuffle=True)
+    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=32)
+
+    return train_dataloader, valid_dataloader
+
+def train(model, dataloader, print_frequency, device):
     """
         trains model using dataloader train + validation profiles
         model object carries with it the criterion, optimizer, epochs
-        print frequency goes by image processed, use_gpu determines device
+        print frequency goes by image processed, device does its part
     """
+    
+    # Parameter Freezing
+    for param in model.parameters():
+        param.requires_grad = False
 
-    # Device logic
-    device = 'cpu'
-    if use_gpu and torch.cude.is_available():
-        mode.cuda()
+    
 
     return -1
 
